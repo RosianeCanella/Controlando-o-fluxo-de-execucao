ed4be3a23a69ad1856ba2f8971b5aa58c9e05438
RosianeCanella <88951010+RosianeCanella@users.noreply.github.com>
2022-04-06 15:46:35 -0300

    Add files via upload
    using System;
    
    namespace Aula02;
    
    class Program
    {
        static void Main(string[] args)
        {
            int mes;
    
            Console.WriteLine("Digite um número do mês:");
            mes = int.Parse(Console.ReadLine());
    
    switch (mes){
        case 1:
        Console.WriteLine("Janeiro");
        break;
        case 2:
        Console.WriteLine("Fevereiro");
        break;
        case 3:
        Console.WriteLine("Março");
        break;
        case 4:
        Console.WriteLine("Abril");
        break;
        case 5:
        Console.WriteLine("Maio");
        break;
        case 6:
        Console.WriteLine("Junho");
        break;
        case 7:
        Console.WriteLine("Julho");
        break;
        case 8:
        Console.WriteLine("Agosto");
        break;
        case 9:
        Console.WriteLine("Setembro");
        break;
        case 10:
        Console.WriteLine("Outubro");
        break;
        case 11:
        Console.WriteLine("Novembro");
        break;
        case 12:
        Console.WriteLine("Dezembro");
        break;
        default:
        Console.WriteLine("Mês inválido!");
        break; 
        }
    }
    }

--- Program.cs
+++ Program.cs
@@ -1,28 +1,56 @@
 ﻿using System;
 
-namespace Aula01
+namespace Aula02;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int valor1;
-            int valor2;
-            int total;
+        int mes;
 
-            Console.WriteLine("Digite primeiro valor");
-            valor1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("O primeiro é:" + valor1);
+        Console.WriteLine("Digite um número do mês:");
+        mes = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o segundo valor:");
-            valor2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("O segundo valor é:" + valor2);
-
-            total = valor1 + valor2;
-
-            Console.WriteLine("A soma dos dois valores é:" + total);
-
-             
-        }
+switch (mes){
+    case 1:
+    Console.WriteLine("Janeiro");
+    break;
+    case 2:
+    Console.WriteLine("Fevereiro");
+    break;
+    case 3:
+    Console.WriteLine("Março");
+    break;
+    case 4:
+    Console.WriteLine("Abril");
+    break;
+    case 5:
+    Console.WriteLine("Maio");
+    break;
+    case 6:
+    Console.WriteLine("Junho");
+    break;
+    case 7:
+    Console.WriteLine("Julho");
+    break;
+    case 8:
+    Console.WriteLine("Agosto");
+    break;
+    case 9:
+    Console.WriteLine("Setembro");
+    break;
+    case 10:
+    Console.WriteLine("Outubro");
+    break;
+    case 11:
+    Console.WriteLine("Novembro");
+    break;
+    case 12:
+    Console.WriteLine("Dezembro");
+    break;
+    default:
+    Console.WriteLine("Mês inválido!");
+    break; 
     }
+}
 }
